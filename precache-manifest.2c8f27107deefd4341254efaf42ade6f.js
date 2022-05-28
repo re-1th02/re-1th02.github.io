658fb9c6c1df01aa4adf3c8c3e77cb90b441ebac
re-1th02 <cs1200407@cse.iitd.ac.in>
2022-05-28 08:52:42 +0530

    Updates

---
+++ precache-manifest.2c8f27107deefd4341254efaf42ade6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc5ed9eb7bc6934b5c7",
+    "url": "/webapp/public/static/css/main.a2084f8e.chunk.css"
+  },
+  {
+    "revision": "8fc5ed9eb7bc6934b5c7",
+    "url": "/webapp/public/static/js/main.356329d9.chunk.js"
+  },
+  {
+    "revision": "d9059f58f867c2dc2ea1",
+    "url": "/webapp/public/static/js/runtime~main.4fae5ed9.js"
+  },
+  {
+    "revision": "70c769342897f30abd0e",
+    "url": "/webapp/public/static/js/2.fda76456.chunk.js"
+  },
+  {
+    "revision": "382c357b7f0cb57d7310",
+    "url": "/webapp/public/static/js/3.160f667a.chunk.js"
+  },
+  {
+    "revision": "126715025a2a13772c73d413bc1a6868",
+    "url": "/webapp/public/index.html"
+  }
+];
